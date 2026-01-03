4419bca90412aab8286e4e5cf886516188f543f5
Abhishek Shinde <your-email@gmail.com>
2026-01-03 17:52:01 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,23 @@
 import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
+// App Config
+const PORT = process.env.PORT || 4000
 const app = express();
-const PORT = process.env.PORT || 4000;
+await connectDB()
 
-await connectDB();
+// Intialize Middlewares
+app.use(express.json())
+app.use(cors())
 
-// ✅ SIMPLE & CORRECT CORS (JWT BASED)
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'https://bg-removel-p6oc.vercel.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'token'],
-  })
-);
+// API routes
+app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
-app.use(express.json());
+app.get('/', (req,res) => res.send("API Working"))
 
-// Routes
-app.use('/api/user', userRouter);
-app.use('/api/image', imageRouter);
-
-app.get('/', (req, res) => res.send('API Working'));
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
