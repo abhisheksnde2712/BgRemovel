0a7246f421184f93d4e8378bd89f239110a15a22
Abhishek Shinde <your-email@gmail.com>
2026-01-03 17:15:39 +0530

    server change

--- server/server.js
+++ server/server.js
@@ -1,50 +1,23 @@
 import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
 // App Config
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
 const app = express();
-await connectDB();
+await connectDB()
 
-// ALLOWED FRONTENDS
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://bg-removel-p6oc.vercel.app",
-];
-
-// CORS FIX (VERY IMPORTANT)
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // Postman / Server requests
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-// ✅ MUST HANDLE PREFLIGHT REQUESTS
-app.options("*", cors());
-
-// JSON
-app.use(express.json());
+// Intialize Middlewares
+app.use(express.json())
+app.use(cors())
 
 // API routes
-app.use("/api/user", userRouter);
-app.use("/api/image", imageRouter);
+app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
-app.get("/", (req, res) => res.send("API Working"));
+app.get('/', (req,res) => res.send("API Working"))
 
-app.listen(PORT, () =>
-  console.log("Server running on port " + PORT)
-);
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
